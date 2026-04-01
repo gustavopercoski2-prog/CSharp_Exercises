9dee5727887cbbade5a60f90cb56737d496df354
gustavopercoski2-prog <gustavopercoski2@gmail.com>
2026-04-01 20:11:53 -0300

    feat: adiciona pasta assets e resolução do exercicio 01

---
+++ Section05_Memoria_Arrays_Listas/Exercicio01_Pensionato/Estudante.cs
@@ -0,0 +1,17 @@
+namespace Exercicio01_Pensionato
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
--- Section05_Memoria_Arrays_Listas/Exercicio01_Pensionato/Program.cs
+++ Section05_Memoria_Arrays_Listas/Exercicio01_Pensionato/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Exercicio01_Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
